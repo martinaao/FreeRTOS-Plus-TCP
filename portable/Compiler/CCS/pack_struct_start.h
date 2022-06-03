cbd3c3b2ca5b65c5eca29ade942162a8961e9b51
martinaao <martin.olsen@hagal.com>
2022-06-03 08:41:18 +0200

    Pop struct pack

--- portable/Compiler/CCS/pack_struct_start.h
+++ portable/Compiler/CCS/pack_struct_start.h
@@ -23,9 +23,5 @@
  *
  *      @brief: The leading compiler directive to pack the following structure to 1 byte
  *
- *      Also suppress an incorrect warning from the CCS compiler:
- *          error #1916-D: definition at end of file not followed by a semicolon or a declarator
  */
-#pragma diag_push
-#pragma diag_suppress=1916
-#pragma pack(1)
+#pragma pack(push, 1)
